67d26173fdba8ff043a5061e14be72bff5993067
Nick <nickrray@gmail.com>
2021-03-28 14:50:24 -0500

    Update text of showHelp to reflect parser instead of scanner

--- src/showHelp.cpp
+++ src/showHelp.cpp
@@ -3,27 +3,24 @@
 void showHelp()
 {
   std::cout << "~ ~ ~ ~ ~ [ Compilers ] ~ ~ ~ ~ ~" << std::endl
-            << " ~ ~ ~ ~ ~ [ Scanner ] ~ ~ ~ ~ ~" << std::endl
+            << "~ ~ ~ ~ ~  [ Parser ] ~ ~ ~ ~ ~" << std::endl
             << std::endl
             << "The only flag used for this program is the one you already used!" << std::endl
             << "    -h  -> Help" << std::endl
             << std::endl
             << "**Run command structure**:" << std::endl
-            << "  ./scanner               -> read from the keyboard until simulated EOF" << std::endl
-            << "  ./scanner < [ _file_ ]  -> redirect from _file_ until simulated EOF, this tests keyboard input" << std::endl
-            << "  ./scanner [ _file_ ]    -> read from _file_.fs" << std::endl
+            << "  ./frontEnd [ _file_ ]    -> read from _file_.fs" << std::endl
             << std::endl
-            << "**NOTE** If you invoke scanner like the third and last example ( ./scanner [ _file_ ] )" << std::endl
-            << "         then the file must be of extension '.fs'." << std::endl
+            << "**NOTE** The file must be of extension '.fs'." << std::endl
             << "         More specifically, if you specify an extension it MUST BE '.fs'" << std::endl
             << "         and if no file extension is specified then scanner will look for [ _file_ ].fs" << std::endl
             << "         Also, the files being provided must be in the same directory as the executable."
             << std::endl
             << "Examples of valid invocations:" << std::endl
-            << "\t./scanner test3.fs" << std::endl
-            << "\t./scanner test3  (Valid IF there is in fact a file 'test3.fs')" << std::endl
+            << "\t./frontEnd test3.fs" << std::endl
+            << "\t./frontEnd test3  (Valid IF there is in fact a file 'test3.fs')" << std::endl
             << "Examples of _invalid_ invocations:" << std::endl
-            << "\t./scanner test2.txt" << std::endl
-            << "\t./scanner test2  (_Invalid_ IF there is NO file 'test2.fs')" << std::endl
+            << "\t./frontEnd test2.txt" << std::endl
+            << "\t./frontEnd test2  (_Invalid_ IF there is NO file 'test2.fs')" << std::endl
             << std::endl;
 }
