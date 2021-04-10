94cb0e20d9f8f90a87d62af72f23bb186bd41b0c
Nick <nickrray@gmail.com>
2021-04-09 23:19:38 -0500

    Add missing default value to function declaration

--- include/tree.hpp
+++ include/tree.hpp
@@ -8,6 +8,6 @@
 #include "node.hpp"
 
 node* initNode(std::string str, node* node);
-void printPreorder(node* treeNode, int level);
+void printPreorder(node* treeNode, int level = 0);
 
 #endif //PARSER_TREE_HPP
